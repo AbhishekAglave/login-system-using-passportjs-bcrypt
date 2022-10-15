60e29805ee7e5833f6dc7a7151158a0e522fae22
abhishek <abhishekaglave@grampower.com>
2022-10-15 21:50:24 +0530

    bhaiya please shi krdo |  crying...);

--- backend/src/models/dbConnection.js
+++ backend/src/models/dbConnection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+
 mongoose
   .connect(process.env.DATABASE_CONNECTION_STRING)
   .then(() => {
@@ -9,4 +10,5 @@ mongoose
     console.log(error);
   });
 
-module.exports = mongoose
+
+module.exports = mongoose;
